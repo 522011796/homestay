3e05c76348119cf9751f913d4261b39f40f78dae
ricky <522011796@qq.com>
2018-07-23 17:04:04 +0800

    框架

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <div class="footer" style="text-align:center;color:#ffffff;font-size:12px;">
-      footer
+      <span>大连网月科技股份有限公司 © 版权所有</span>
+      <span>辽ICP备12008717号</span>
     </div>
   </div>
 </template>
