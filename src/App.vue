a908e80d687ec7219ef155b6dbbf456aeb4a4204
ricky <522011796@qq.com>
2018-07-23 10:58:36 +0800

    框架

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div style="min-width: 1280px">
-    <!--<div style="background: #3ab573">
-      <div style="margin:0 35px;">
-        <router-view></router-view>
-      </div>
-    </div>-->
     <router-view></router-view>
+
+    <Modal
+      title="测试内容，如果你看到这个提示，表示浏览器版本验证生效"
+      v-model="ieVersion"
+      :closable="false"
+      :mask-closable="false"  class="app-footer">
+
+      <div class="text-center">
+        <div>系统监测到浏览器为ie内核且版本偏低！</div>
+        <div>推荐使用谷歌、火狐或者其他双核极速模式！</div>
+        <div>如果你使用的是360、qq、搜狗等双核浏览器，请在顶部切换为极速模式访问！</div>
+      </div>
+
+      <div slot="footer"></div>
+    </Modal>
   </div>
 </template>
 
@@ -16,7 +26,27 @@ export default {
   components: { Head },
   data () {
     return {
-      msg: ""
+      msg: "",
+      ieVersion:false
+    }
+  },
+  created(){
+    var getExplorer = (function() {
+      var explorer = window.navigator.userAgent,
+        compare = function(s) { return (explorer.indexOf(s) >= 0); },
+        ie11 = (function() { return ("ActiveXObject" in window) })();
+      if (compare("MSIE") || ie11) { return 'ie'; }
+      else if (compare("Firefox") && !ie11) { return 'Firefox'; }
+      else if (compare("Chrome") && !ie11) { return 'Chrome'; }
+      else if (compare("Opera") && !ie11) { return 'Opera'; }
+      else if (compare("Safari") && !ie11) { return 'Safari'; }
+    })()
+
+    //注意：getExplorer返回结果是一个字符串，如判断是否为IE浏览器
+    if (getExplorer == 'ie') {
+      //alert('当前浏览器版本：IE');
+      this.ieVersion = true;
+
     }
   }
 }
